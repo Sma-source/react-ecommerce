ef427f7e07167bd0d048d25821eedc530531e622
Sma-source <rabhi.smain@gmail.com>
2023-06-03 20:42:30 +0200

    create routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Home from "./pages/Home";
+import ProductDetails from "./pages/ProductDetails";
+
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import Footer from "./components/Footer";
+
 function App() {
   return (
     <>
-      <h1 className="text-leading font-bold text-3xl text-black underline">
-        TailwindCSS
-      </h1>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product/:id" element={<ProductDetails />} />
+        </Routes>
+        <Sidebar />
+        <Footer />
+      </Router>
     </>
   );
 }
