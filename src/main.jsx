dc33adf170e4d70a5b15959cc51f027472fbf543
Sma-source <rabhi.smain@gmail.com>
2023-06-03 21:12:42 +0200

    import product provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// product provider
+import ProductProvider from "./context/ProductContext";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ProductProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ProductProvider>
+);
