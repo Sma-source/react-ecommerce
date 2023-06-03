795bdadb631b16f163e98422dabb66279b9eb39a
Sma-source <rabhi.smain@gmail.com>
2023-06-03 21:24:35 +0200

    import ProductContext

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useContext } from "react";
+
+//import product context
+import { ProductContext } from "../context/ProductContext";
 
 const Home = () => {
   return <div>Home</div>;
