ef93ceff2945860dbd70ae1c9d2dae7129be2486
Sma-source <rabhi.smain@gmail.com>
2023-06-03 21:00:47 +0200

    add product state

--- src/context/ProductContext.jsx
+++ src/context/ProductContext.jsx
@@ -3,6 +3,9 @@ import React, { createContext, useState, useEffect } from "react";
 //create context
 export const ProductContext = createContext();
 
+// product state
+const [products, setProducts] = useState([]);
+
 const ProductProvider = ({ children }) => {
   return <ProductContext.Provider>{children}</ProductContext.Provider>;
 };
