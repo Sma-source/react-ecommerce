4dea64bf9535ce74e8f66337615b82a2ede1940a
Sma-source <rabhi.smain@gmail.com>
2023-06-03 20:16:29 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-leading font-bold text-3xl text-black underline">
+        TailwindCSS
+      </h1>
+    </>
+  );
+}
+
+export default App;
