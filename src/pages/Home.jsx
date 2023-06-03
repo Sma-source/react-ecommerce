e9b047997e1c9e4931874958e886e2d2c0d46443
Sma-source <rabhi.smain@gmail.com>
2023-06-03 22:31:48 +0200

    filter product and display Product component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,33 @@ import React, { useContext } from "react";
 
 //import product context
 import { ProductContext } from "../context/ProductContext";
+import Product from "../components/Product";
 
 const Home = () => {
-  return <div>Home</div>;
+  // get products from product context
+  const { products } = useContext(ProductContext);
+
+  // filter products to get only clothing category
+  const filterProducts = products.filter((item) => {
+    return (
+      item.category === "men's clothing" || item.category === "women's clothing"
+    );
+  });
+  // console.log(filterProducts);
+
+  return (
+    <>
+      <section className="py-16">
+        <div className="max-w-7xl mx-auto py-16 px-4 overflow-hidden sm:py-24 sm:px-6 lg:px-8">
+          <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 lg:gap-x-8">
+            {filterProducts.map((product) => {
+              return <Product key={product.id} product={product} />;
+            })}
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Home;
