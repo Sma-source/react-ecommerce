aa8b71fddfb6de3d0454e9e745698b498ac25582
Sma-source <rabhi.smain@gmail.com>
2023-06-04 15:00:07 +0200

    import sidebar provider

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,15 @@ import "./index.css";
 
 // product provider
 import ProductProvider from "./context/ProductContext";
+// import sidebar provider
+import SidebarProvider from "./context/SidebarContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <ProductProvider>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </ProductProvider>
+  <SidebarProvider>
+    <ProductProvider>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </ProductProvider>
+  </SidebarProvider>
 );
