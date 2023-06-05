ecace7263a960c680a6218d35c729754f3730380
Sma-source <rabhi.smain@gmail.com>
2023-06-05 20:39:31 +0200

    import Hero component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import React, { useContext } from "react";
 //import product context
 import { ProductContext } from "../context/ProductContext";
 import Product from "../components/Product";
+import Hero from "../components/Hero";
 
 const Home = () => {
   // get products from product context
@@ -18,6 +19,7 @@ const Home = () => {
 
   return (
     <>
+      <Hero />
       <section className="mx-auto">
         <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
           <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
