c507e41f872a24d5221d34e313fdd94ccf190bb2
Sma-source <rabhi.smain@gmail.com>
2023-06-05 20:43:20 +0200

    change btn color

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -36,7 +36,7 @@ const Product = ({ product }) => {
         <button
           onClick={() => addToCart(product, id)}
           type="button"
-          className="relative cursor-pointer w-full flex bg-gray-100 border border-transparent rounded-md py-2 px-8 items-center justify-center text-sm font-medium text-gray-900 hover:bg-gray-200"
+          className="relative cursor-pointer w-full flex bg-[#F5E6E0] border border-transparent rounded-md py-2 px-8 items-center justify-center text-sm font-medium text-gray-900 hover:bg-gray-600 hover:text-white"
         >
           Add to bag<span className="sr-only">, {title}</span>
         </button>
