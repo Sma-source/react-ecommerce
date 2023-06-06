a50a3fcf1eb2378cb6d2477b77476c72a8fd9c3e
Sma-source <rabhi.smain@gmail.com>
2023-06-06 14:11:01 +0200

    add ui footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="bg-gray-800 py-12">
+      <div className="container mx-auto">
+        <p className="text-white text-center">
+          Copyright &copy; Ecommerce Shop 2023. All rights reserved.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
