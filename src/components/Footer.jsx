f1b6038d016599a518f3800420ef8f787c8b1f93
Sma-source <rabhi.smain@gmail.com>
2023-06-07 12:31:12 +0200

    modify ui

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="bg-gray-800 py-12">
+    <div className="bg-gray-800 py-16">
       <div className="container mx-auto">
         <p className="text-white text-center">
           Copyright &copy; Ecommerce Shop 2023. All rights reserved.
