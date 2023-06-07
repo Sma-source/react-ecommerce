e6a2b9db8142924818a6e18311570a659e44ad05
Sma-source <rabhi.smain@gmail.com>
2023-06-07 21:20:00 +0200

    modify hero ui

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import woman from "../assets/woman2.png";
 import { Link } from "react-router-dom";
+import Banner from "./Banner";
 
 const Hero = () => {
   const offers = [
@@ -46,70 +47,71 @@ const Hero = () => {
     //     </div>
     //   </div>
     // </section>
-
-    <div className="bg-white">
-      <div className="flex flex-col border-b border-gray-200 lg:border-0">
-        <nav aria-label="Offers" className="order-last lg:order-first">
-          <div className="max-w-7xl mx-auto lg:px-8">
-            <ul
-              role="list"
-              className="grid grid-cols-1 divide-y divide-gray-200 lg:grid-cols-3 lg:divide-y-0 lg:divide-x"
-            >
-              {offers.map((offer) => (
-                <li key={offer.name} className="flex flex-col">
-                  <a
-                    href={offer.href}
-                    className="relative flex-1 flex flex-col justify-center bg-white py-6 px-4 text-center focus:z-10"
-                  >
-                    <p className="text-sm text-gray-500">{offer.name}</p>
-                    <p className="font-semibold text-gray-900">
-                      {offer.description}
-                    </p>
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </nav>
-
-        <div className="relative">
-          <div
-            aria-hidden="true"
-            className="hidden absolute w-1/2 h-full bg-gray-100 lg:block"
-          />
-          <div className="relative bg-gray-100 lg:bg-transparent">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 lg:grid lg:grid-cols-2">
-              <div className="max-w-2xl mx-auto py-24 lg:py-64 lg:max-w-none">
-                <div className="lg:pr-16">
-                  <h1 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl xl:text-6xl">
-                    Focus on what matters
-                  </h1>
-                  <p className="mt-4 text-xl text-gray-600">
-                    All the charts, datepickers, and notifications in the world
-                    can't beat checking off some items on a paper card.
-                  </p>
-                  <div className="mt-6">
+    <>
+      <div className="bg-white">
+        <div className="flex flex-col border-b border-gray-200 lg:border-0">
+          <nav aria-label="Offers" className="order-last lg:order-first">
+            <div className="max-w-7xl mx-auto lg:px-8">
+              <ul
+                role="list"
+                className="grid grid-cols-1 divide-y divide-gray-200 lg:grid-cols-3 lg:divide-y-0 lg:divide-x"
+              >
+                {offers.map((offer) => (
+                  <li key={offer.name} className="flex flex-col">
                     <a
-                      href="#"
-                      className="inline-block bg-indigo-600 border border-transparent py-3 px-8 rounded-md font-medium text-white hover:bg-indigo-700"
+                      href={offer.href}
+                      className="relative flex-1 flex flex-col justify-center bg-white py-6 px-4 text-center focus:z-10"
                     >
-                      Shop Productivity
+                      <p className="text-sm text-gray-500">{offer.name}</p>
+                      <p className="font-semibold text-gray-900">
+                        {offer.description}
+                      </p>
                     </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </nav>
+
+          <div className="relative">
+            <div
+              aria-hidden="true"
+              className="hidden absolute w-1/2 h-full bg-gray-100 lg:block"
+            />
+            <div className="relative bg-gray-100 lg:bg-transparent">
+              <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 lg:grid lg:grid-cols-2">
+                <div className="max-w-2xl mx-auto py-24 lg:py-64 lg:max-w-none">
+                  <div className="lg:pr-16">
+                    <h1 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl xl:text-6xl">
+                      Discover Fashion's Finest
+                    </h1>
+                    <p className="mt-4 text-xl text-gray-600">
+                      Elevate Your Wardrobe with our Exclusive Collection of
+                      Fashion-forward Clothing and Accessories
+                    </p>
+                    <div className="mt-6">
+                      <a
+                        href="#"
+                        className="inline-block bg-indigo-600 border border-transparent py-3 px-8 rounded-md font-medium text-white hover:bg-indigo-700"
+                      >
+                        Shop Now!
+                      </a>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="w-full h-48 sm:h-64 lg:absolute lg:top-0 lg:right-0 lg:w-1/2 lg:h-full">
-            <img
-              src="src/assets/fashion.jpg"
-              alt=""
-              className="w-full h-full object-center object-cover"
-            />
+            <div className="w-full h-48 sm:h-64 lg:absolute lg:top-0 lg:right-0 lg:w-1/2 lg:h-full">
+              <img
+                src="src/assets/fashion.jpg"
+                alt=""
+                className="w-full h-full object-center object-cover"
+              />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
